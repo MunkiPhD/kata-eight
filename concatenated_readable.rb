d8af27efedcb1bab5fedff0c398aae81b9a2cf34
MunkiPhD <munkiphd@gmail.com>
2013-06-17 22:24:06 -0400

    finished algorithm

--- concatenated_readable.rb
+++ concatenated_readable.rb
@@ -3,7 +3,42 @@ class ConcatenatedReadable
 		@dictionary = dictionary
 	end
 
-	def six_letter_words
-		@dictionary.select { |word| word.length == 6 }
+
+	def words_of_length(length)
+		@dictionary.select { |word| word.length == length }
+	end
+
+
+	def find_concatenated_words_equaling_length(length)
+		# get the subset of words that are equal to this length
+		subset = words_of_length(length)
+
+		# we need to find words of length X made up of two smaller words
+		# assumptions are that subwords must be at least two characters long, so we're basically looking for where the third and fourth character sit
+		#  1234 56
+		#  123 456
+		#  12 3456
+
+		# the number of sub words we'll have to try is the number of times we'll have to shift the characters where we still satisfy that a word must be at least 2 characters
+		possibilities = length - 4
+		if possibilities == 0
+			return []
+		end
+
+
+		valid_words = []
+		subset.each do |word|
+			for i in 0..(possibilities - 1)
+				word_one = word.slice(0, 2 + i)
+				word_two = word.slice(word_one.length, word.length - 1)
+				puts "#{word_one} .. #{word_two}"
+				if @dictionary.include?(word_one) && @dictionary.include?(word_two)
+					valid_words << word
+					puts "match found with: #{word_one} .. #{word_two}"
+				end
+			end
+		end
+
+		valid_words
 	end
 end
--- concatenated_readable_spec.rb
+++ concatenated_readable_spec.rb
@@ -15,9 +15,21 @@ describe ConcatenatedReadable do
 =end
 
 	it 'finds all six letter words' do
-		six_letter_words = readable.six_letter_words
+		six_letter_words = readable.words_of_length(6)
 		%w(albums barely befoul convex tigers pandas).each do |word|
 			expect(six_letter_words.include?(word)).to be_true
+		end
+	end
+
+	it 'returns nothing when length is less than 4' do
+		expect(readable.find_concatenated_words_equaling_length(3)).to eq []
+	end
+
+	it 'finds six letter words that are concatenated' do
+		words = readable.find_concatenated_words_equaling_length(6)
+		expect(words.length).to eq 4
+		%w(albums barely befoul convex).each do |word|
+			expect(words.include?(word)).to be_true
 		end
 	end
 end
